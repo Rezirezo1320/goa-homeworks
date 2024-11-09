0148883e53d376f3e007b3088741ea60d8eb9bfb
Rezirezo1320 <kurdadzerezi2@gmail.com>
2024-11-09 16:02:56 +0400

    commit

---
+++ day8/homework/hw.py
@@ -0,0 +1,41 @@
+num1=12
+num2 =45
+num3 = 42
+num4=57
+num5=69
+print((num1 + num2) / num3 - num4*num5 + num1**num3 % num2)
+num1=12
+num2 =4
+num3 = 56
+num4=67
+num5=6
+print((num1 - num2) / num3 + num4*num5 + num1**num3 % num2)
+num1=19
+num2 =6
+num3 = 54
+num4=45
+num5=4
+print((num1 + num2) / num3 - num4**num5 + num1*num3 % num2)
+num1=8
+num2 =7
+num3 = 56
+num4=55
+num5=5
+print((num1 + num2) % num3 - num4*num5 + num1**num3 /num2)
+num1=98
+num2 =49
+num3 = 42
+num4=5
+num5=69
+print((num1 + num2) / num3 - num4**num5 + num1*num3% num2)
+num1=12
+num2 =45
+num3 = 89
+num4=5
+num5=45
+print((num1 + num2) / num3 - num4*num5 + num1**num3 % num2)
+num1=12
+num2 =35
+num3 = 42
+num4=34
+num5=64
