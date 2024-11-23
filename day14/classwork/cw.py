a908a36d19885e6fb2cb0370ff2d9da8b101c4a5
Rezirezo1320 <kurdadzerezi2@gmail.com>
2024-11-23 12:05:54 +0400

    commit

---
+++ day14/classwork/cw.py
@@ -0,0 +1,18 @@
+for i in range(20)
+ print("rezi kurdadze")
+
+
+ num2 = input(int("enter your number"))
+ num3 = input(int("enter your number"))
+for a in range(10)
+    num2**num2
+    num3**num3
+
+num4,num5 = 6 =9
+
+
+number  = input(int("enter your number"))
+for r in range(0,number)
+    print(range*2)
+
+
---
+++ day14/homework/hw.py
@@ -0,0 +1,32 @@
+range(20,50)
+
+range(100,150)
+
+range(20,50)
+
+range(11,31,2)
+
+for r in range(10):
+ print("rezi kurdadze")
+
+
+i = range(10,30)
+range(i/2)
+
+u = range(40,60)
+range(u ** 3)
+
+
+num1 = input("enter your number")
+num2  = input("enter your number")
+num3  = input("enter your number")
+num4  = input("enter your number")
+num5  = input("enter your number")
+
+
+for u in range(num1,num2,num3,num4,num5))
+ 
+
+
+
+#როდესაც ერთიდა იგივე წინადადება გვაქვს დასაწერი არ არის კარგი წესი მისი ციკლის გარეშე დაწერა
