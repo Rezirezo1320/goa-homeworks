3df614080a50e1f39cebefb5c4b90bdc6596a335
Rezirezo1320 <kurdadzerezi2@gmail.com>
2025-01-05 16:19:21 +0400

    commit

---
+++ day26/classwork/cw.py
@@ -0,0 +1,15 @@
+def greet (myname):
+    print("გამარჯობა," myname)
+    greet("rezi")
+
+
+
+def  manual_range (start, end, step) :
+    
+    for i in manual_range:
+     if i %2 ==0 :
+        print(i)
+
+manual_range(23,23,22)
+manual_range(21,22, 44)
+manual_range(44,66,98)
