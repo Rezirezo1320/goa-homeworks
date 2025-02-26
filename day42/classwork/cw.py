58719ff5f0e2df813e13db2191cff5f766ada50e
Rezirezo1320 <kurdadzerezi2@gmail.com>
2025-02-26 14:49:51 +0400

    commit

---
+++ day42/classwork/cw.py
@@ -0,0 +1,24 @@
+#შექმენით dictionary,
+#  სადაც იქნება შემდეგი გასაღებები:
+#  name, surname, academy, fav-color, city, goa-student, age, fav-programming-lang.
+
+
+
+dict1={
+    "name":"rezi",
+    "surname":"kurdadze",
+    "academy":"goa",
+    "fav-color":"yellow",
+    "goa-student":"true",
+    "age":"14",
+    "fav-programming-lang":"html",
+}
+
+
+print(dict1["name"])
+print(dict1["surname"])
+print(dict1["academy"])
+print(dict1["fav-color"])
+print(dict1["goa-student"])
+print(dict1["age"])
+print(dict1["fav-programming-lang"])
---
+++ day42/homework/hw.py
@@ -0,0 +1,21 @@
+# Creating a dictionary
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+
+# Printing all values using values() method
+print("Values:", my_dict.values())
+
+
+# Creating a dictionary
+my_dict = {'brand': 'Toyota', 'model': 'Camry', 'year': 2022}
+
+# Printing all key-value pairs using items() method
+print("Key-Value Pairs:", my_dict.items())
+
+
+
+# Creating a dictionary
+person = {'name': 'John', 'age': 30, 'profession': 'Engineer'}
+
+# Iterating over the dictionary using items() method
+for key, value in person.items():
+    print(f"{key}: {value}")
